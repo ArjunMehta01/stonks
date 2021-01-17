429c96a0d94c90860605534f82be35c3c2024396
arjun <arjun.mehta1001@gmail.com>
2021-01-17 00:02:59 -0700

    ml stuf for test

--- machineLearning/ml.py
+++ machineLearning/ml.py
@@ -17,11 +17,6 @@ import pandas
 # train_data, test_data = torchtext.data.TabularDataset.splits(path='mydata', train ='train.csv', test='test.csv', format='csv', fields=fields)
 
 
-data = pandas.read_csv('posts.csv')
-
-for row in data df.iterrows():
-    print(row)
-
-data['Sentiment'] = sentiment
+data = pandas.read_csv('machineLearning/posts.csv')
 
 
